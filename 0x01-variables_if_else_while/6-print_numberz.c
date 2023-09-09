c8e0f47c6bf2474465feb63919c8c0d13401e01b
UserStine <justineaddo15@gmail.com>
2023-09-09 00:42:03 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - a pprogram that prints numbers from 0 to 9.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
